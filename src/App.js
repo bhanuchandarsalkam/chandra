6aff1cb2902f86f800199577672c63f825aa7db1
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-09-18 15:12:49 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "./components/form";
+import './style.css'
+const App=()=>{
+
+    return(
+        <div>
+       <Form/>
+        </div>
+    )
+}
+export default App;
